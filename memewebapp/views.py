56977b6641673f4ff7f16c9c98cf755922fb06e8
kalilinux <bhavinpatil28@gmail.com>
2021-02-26 23:36:03 +0530

    first commit

---
+++ memewebapp/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+## Create your views here.
+from .forms import  CreateUserForm
+import json
+import requests
+
+
+def loginPage(request):
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password =request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+
+			if user is not None:
+				login(request, user)
+				return redirect('home')
+			else:
+				messages.info(request, 'Username OR password is incorrect')
+
+		context = {}
+		return render(request, 'accounts/login.html', context)
+
+def logoutUser(request):
+	logout(request)
+	return redirect('login')
+
+
+@login_required(login_url='login')
+def home(request):
+	response = requests.get('https://api.imgflip.com/get_memes')
+	data = response.json()
+	return render(request, 'accounts/dashboard.html', {'response':data})
+	
+
+	
---
+++ readme.txt
@@ -0,0 +1,2 @@
+root
+Admin@28
